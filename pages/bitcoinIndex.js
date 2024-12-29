668852929fbe5adc5bcde89522d6f53e1595d765
abdulbasit-suleiman <abdulbasitsuleiman28@gmail.com>
2024-12-29 15:34:54 +0100

    bt active

--- pages/bitcoinIndex.js
+++ pages/bitcoinIndex.js
@@ -3,14 +3,7 @@ import { generatePhrases } from '../shuffle';  // Function to generate shuffled 
 import { generateBitcoinAddress, checkBitcoinAddress } from '../bitcoin';  // Bitcoin address related functions
 
 const wordList = [
-  "abandon", "ability", "able", "about", "above", "absent", "absorb", "abstract", 
-  "absurd", "abuse", "access", "accident", "account", "accuse", "achieve", "acid", 
-  "acoustic", "acquire", "across", "act", "action", "actor", "actress", "actual", 
-  "adapt", "add", "addict", "address", "adjust", "admit", "adult", "advance", 
-  "advice", "aerobic", "affair", "afford", "afraid", "again", "age", "agent", 
-  "agree", "ahead", "aim", "air", "airport", "aisle", "alarm", "album", 
-  "alcohol", "alert", "alien", "all", "alley", "allow", "almost", "alone", 
-  "alpha", "already", "also", "alter", "always", "amateur", "amazing", "among", 
+   "alter", "always", "amateur", "amazing", "among", 
   "amount", "amused", "analyst", "anchor", "ancient", "anger", "angle", "angry", 
   "animal", "ankle", "announce", "annual", "another", "answer", "antenna", "antique", 
   "anxiety", "any", "apart", "apology", "appear", "apple", "approve", "april", 
